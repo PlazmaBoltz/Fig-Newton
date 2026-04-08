e06604ccd806a96a5de9ff7c6a472ad260d68eda
PlazmaBoltz <gjcerruti@outlook.com>
2026-04-08 09:59:29 -0400

    Initial commit

---
+++ Fig-Newton/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Brutal.Numerics;
+using Brutal.ImGuiApi;
+using StarMap.API;
+using PB.FigNewton;
+
+namespace Fig_Newton;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
