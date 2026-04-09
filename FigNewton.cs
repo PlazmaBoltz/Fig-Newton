a79e9f0e0d265788cf50ceeaff21b9b583c23a8a
PlazmaBoltz <gjcerruti@outlook.com>
2026-04-09 11:07:16 -0400

    various fixes

--- FigNewton.cs
+++ FigNewton.cs
@@ -2,6 +2,8 @@
 using Brutal.Numerics;
 using Brutal.ImGuiApi;
 using StarMap.API;
+using KSA;
+using System.Security.Cryptography;
 
 namespace Fig_Newton;
 
@@ -9,81 +11,78 @@ namespace Fig_Newton;
 public class Mod
 {
     public bool ImmediateUnload => false;
-
     private bool _isInitialized;
     private bool _isDisposed;
     private bool _windowVisible;
-    private FNSubmod _submod = null!;
+    private List<Astronomical> astronomicalList;
+    private CelestialSystem celestialSystem;
+    private double astMass;
+    private double otherMass;
 
     [StarMapImmediateLoad]
     public void OnImmediateLoad() { }
 
     [StarMapAllModsLoaded]
     public void OnFullyLoaded()
-    {
-        try
-        {
-            _submod = new FNSubmod();
-            Patcher.Patch();
-            _submod.Initialize();
-            _isInitialized = true;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"fig-newton: Error during initialization: {ex.Message}");
-        }
+    {        
+        _isInitialized = true;
     }
 
     [StarMapBeforeGui]
     public void OnBeforeUi(double dt)
     {
         if (!_isInitialized || _isDisposed) return;
-        _submod.Update(dt);
 
-        var astronomicalList = celestialSystem.All.GetList();
+        var astronomicalList = KSA.Universe.CurrentSystem.All.GetList();
 
-        for (astronomicalList.MoveToStart(); astronomicalList.IsValid(); astronomicalList.MoveNext())
+        foreach (var astronomical in astronomicalList)
         {
-            var astronomical = astronomicalList.Current();
             if (astronomical == null) continue;
-            if (astronomical.HasOrbit())
-            {
-                astronomical.StateVectors = astronomical.StateVectors;
-            };
-            _submod.UpdateAstronomical(astronomical);
         }
     }
-
-    [StarMapOnFrame]
-    public void OnFrame(double dt)
+    [StarMapAfterGui]
+    public void OnAfterGui(double dt)
     {
         if (!_isInitialized || _isDisposed) return;
-        for (astronomicalList.MoveToStart(); astronomicalList.IsValid(); astronomicalList.MoveNext())
+        foreach (var astronomical in astronomicalList)
         {
-            var astronomical = astronomicalList.Current();
             if (astronomical == null) continue;
             if (astronomical.HasOrbit())
             {
-                for (astronomicalList.MoveToStart(); astronomicalList.IsValid(); astronomicalList.MoveNext())
+                foreach (var other in astronomicalList)
                 {
-                    var other = astronomicalList.Current();
-                    if (other == null || other == astronomical) continue;
-                    if (other.Mass >= 0)
+                    if (other is Celestial celestial)
                     {
-                        var r = (astronomical.x, astronomical.y, astronomical.z) - (other.x, other.y, other.z);
-                        var distance = r.Magnitude();
+                        var otherMass = celestial.Mass;
+                    }
+                    else
+                    {
+                        break;
+                    };
+                    if (astronomical is Celestial celestial2)
+                    {
+                        var astMass = celestial2.Mass;
+                    }
+                    else
+                    {
+                        break;
+                    };
+                    if (other == null || other == astronomical) continue;
+                    if (other == astronomical) break;
+                    if (otherMass >= 0)
+                    {
+                        var r = astronomical.GetPositionEcl() - other.GetPositionEcl();
+                        var distance = astronomical.DistanceTo(other);
                         if (distance > 0)
                         {
-                            var forceMagnitude = 0.0000000000337384*(astronomical.Mass * other.Mass)/(distance * distance)*dt;
+                            var forceMagnitude = 0.0000000000667384*(astMass * otherMass)/(distance * distance)*dt;
                             var forceDirection = r.Normalized();
                             var force = forceDirection * forceMagnitude;
-                            astronomical.StateVectors += force / astronomical.Mass;
+                            celestial2.Orbit.Create(celestial2.Orbit.Parent, celestial2.Orbit, celestial2.Orbit.OrbitLineColor, new StateVectors(new SimTime(0)+celestial2.Orbit.StateVectors.StateTime,new double3(0.0,0.0,0.0)+celestial2.Orbit.StateVectors.PositionCci,force/astMass+celestial2.Orbit.StateVectors.VelocityCci, new TrueAnomaly(0) + celestial2.Orbit.StateVectors.TrueAnomaly));
                         }
                     };
                 };
             };
-        } 
-        _submod.Update(dt);
+        }
     }
-
 }
